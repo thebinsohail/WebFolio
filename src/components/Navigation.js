a3324590d3a7067004697b6297a9d4cde8e46d49
Anas Bin Sohail <anassohail.as@gmail.com>
2020-08-29 04:49:58 +0500

    added navbar and home component

---
+++ src/components/Navigation.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
+import { DiCode } from "react-icons/di";
+const Navigation = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar light expand="md">
+            <NavbarBrand href="/" ><DiCode size="3em" color="#5677D4"/>Anas Bin Sohail</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+            <NavLink href="/"><button className="btn btn-primary home">Home</button></NavLink>
+                        </NavItem>
+                        <NavItem>
+                <NavLink href="/about"><button className="btn btn-primary">About</button></NavLink>
+                        </NavItem>
+                        <NavItem>
+                <NavLink href="/services"><button className="btn btn-primary">Services</button></NavLink>
+                        </NavItem>
+                        <NavItem>
+                  <NavLink href="/contact"><button className="btn btn-primary">Contact</button></NavLink>
+                        </NavItem>
+                  
+                  
+                    </Nav>
+                  
+                   
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Navigation;
