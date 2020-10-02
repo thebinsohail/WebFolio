daac40ee86df96a70e98ed454ee0947ef9869b54
DevHyperCoder <arduinoleo88@gmail.com>
2020-10-02 10:04:02 +0530

    ✏️ Fix typo ⚡️ Formatting

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,6 @@
 export const TEXTS = [
     "Hello there!",
     "I'm Anas Bin Sohail"
-
 ];
 
 export const profession = "Developer | Software Engineer"
@@ -19,18 +18,18 @@ export const genderOptions = [
     { key: 'm', text: 'Male', value: 'male' },
     { key: 'f', text: 'Female', value: 'female' },
     { key: 'o', text: 'Other', value: 'other' },
-  ]
+]
 
-  export const info = {
+export const info = {
     profession: ' Developer | Software Engineer',
     question: 'Who Am I?',
     personal: "My name is Anas Bin Sohail and I am a Full Stack (MERN) developer",
-    intro: "educated and a motivated software engineering student and a quick learner, quick to ramp up with any existing development platform, and perform design, coding, debugging, and testing application and system software. Excellent interpersonal as well as communication skills and enjoys a high-pressure team environment"
+    intro: "Educated and a motivated software engineering student and a quick learner, quick to ramp up with any existing development platform, and perform design, coding, debugging, and testing application and system software. Excellent interpersonal as well as communication skills and enjoys a high-pressure team environment"
 }
+
 export const education = {
     bachelors: 'Bachelor of Science in Software Engineering',
     uni: 'Sir Syed University of Engg & Technology',
     year: '2019-2022',
     college: 'Govt National College Karachi'
-
 }
